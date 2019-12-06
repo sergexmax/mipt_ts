a187589d1a836065e191c269a720a75fbd221cca
Sergei Puchinin <sergexmax@yandex.ru>
2019-12-06 15:00:06 +0300

    Add transmission by signals

--- ts.c
+++ ts.c
@@ -1,22 +1,45 @@
+#define _GNU_SOURCE /* For sigset_t. */
+
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 #define FILENAME_MAX_LENGTH 64
+#define PATHNAME_MAX_LENGTH 1024
+
+#define ATOMIC_BLOCK_SIZE 1024
+
+#define DATA_PATH "/home/serge/data/"
 
 /* Error codes. */
 enum {
         WRONG_USAGE = -2,
 };
 
+static int bit;
+
+void
+child_sigusr1(int nsig); /* Receive signal that parent received previous bit. */
+void
+parent_sigusr1(int nsig); /* Receive bit '0'*/
+void
+parent_sigusr2(int nsig); /* Receive bit '1' */
+void
+sigchld(int nsig); /* Receive signal that child finished. */
+
 int
 main(int argc, char *argv[])
 {
         char file_name[FILENAME_MAX_LENGTH];
+        pid_t ppid;
         pid_t chpid;
 
         /* Get file name. */
@@ -28,6 +51,9 @@ main(int argc, char *argv[])
                 strcpy(file_name, argv[1]);
         }
 
+        /* Get parent pid. */
+        ppid = getpid();
+
         /* Create child. */
         chpid = fork(); /* Child pid. */
         if (chpid < 0) {
@@ -37,17 +63,117 @@ main(int argc, char *argv[])
 
         /* CHILD. */
         if (chpid == 0) {
-                const char name[] = "Child";
-                printf("Hello World! I am the %s (%d).\n", name, getpid());
-                printf("%s\n", file_name);
+                // const char name[] = "Child";
+                int file_fd_read;
+                char file_path[PATHNAME_MAX_LENGTH];
+                char *buf;
+                int read_size;
+                sigset_t mask;
+                sigset_t oldmask;
+
+                // printf("Hello World! I am the %s (%d).\n", name, getpid());
+
+                /* Open file for reading. */
+                sprintf(file_path, "%s%s", DATA_PATH, file_name);
+                if ((file_fd_read = open(file_path, O_RDONLY | O_NONBLOCK)) < 0) {
+                        perror("open");
+                        exit(errno);
+                }
+
+                /* Prepare signals. */
+                signal(SIGUSR1, &child_sigusr1);
+                sigemptyset(&mask);
+                sigaddset(&mask, SIGUSR1);
+                sigprocmask(SIG_BLOCK, &mask, &oldmask);
+
+                /* Read file. */
+                if ((buf = malloc(ATOMIC_BLOCK_SIZE)) == NULL) {
+                        perror("malloc");
+                        exit(errno);
+                }
+                while((read_size = read(file_fd_read, buf, ATOMIC_BLOCK_SIZE)) != 0) {
+                        if (read_size < 0) {
+                                perror("read");
+                                exit(errno);
+                        }
+                        for (int i = 0; i < read_size; ++i) {
+                                for (int j = 0; j < 8; ++j) {
+                                        if ((buf[i] & (1 << (7 - j))) == 0)
+                                                kill(ppid, SIGUSR1);
+                                        else
+                                                kill(ppid, SIGUSR2);
+                                        sigsuspend(&oldmask);
+                                }
+                        }
+                }
+
+                free(buf);
+                if (close(file_fd_read)) {
+                        perror("close");
+                        exit(errno);
+                }
         }
         /* PARENT */
         else { /* chpid > 0 */
-                const char name[] = "Parent";
-                printf("Hello World! I am the %s (%d).\n", name, getpid());
+                // const char name[] = "Parent";
+                sigset_t mask;
+                sigset_t oldmask;
+                char c;
+
+                // printf("Hello World! I am the %s (%d).\n", name, getpid());
+
+                /* Prepare signals. */
+                signal(SIGUSR1, &parent_sigusr1);
+                signal(SIGUSR2, &parent_sigusr2);
+                signal(SIGCHLD, &sigchld);
+                sigemptyset(&mask);
+                sigaddset(&mask, SIGUSR1);
+                sigaddset(&mask, SIGUSR2);
+                sigaddset(&mask, SIGCHLD);
+                sigprocmask(SIG_BLOCK, &mask, &oldmask);
+
+                /* Write file. */
+                while(true) {
+                        c = '\0';
+                        for (int i = 0; i < 8; ++i) {
+                                sigsuspend(&oldmask);
+                                c |= bit << (7 - i);
+                                kill(chpid, SIGUSR1);
+                        }
+                        write(1, &c, 1);
+                }
         }
 
-        waitpid(chpid, NULL, 0);
+        exit(EXIT_FAILURE);
+}
 
+/* Receive signal that parent received previous bit. */
+void
+child_sigusr1(int nsig)
+{
+        // printf("Received signal from parent.\n");
+}
+
+/* Receive bit '0'*/
+void
+parent_sigusr1(int nsig)
+{
+        // printf("Received bit '0'.\n");
+        bit = 0;
+}
+
+/* Receive bit '1' */
+void
+parent_sigusr2(int nsig)
+{
+        // printf("Received bit '1'.\n");
+        bit = 1;
+}
+
+/* Receive signal that child finished. */
+void
+sigchld(int nsig)
+{
+        // printf("Received signal from child.\n");
         exit(EXIT_SUCCESS);
 }
